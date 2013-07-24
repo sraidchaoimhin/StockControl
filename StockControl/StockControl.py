361ebb49d911a210f132a3da997060c1a08dc9f7
M Mac <sraidchaoimhin@gmail.com>
2013-07-24 15:59:45 +0100

    Added skeleton framework for project

--- StockControl/StockControl.py
+++ StockControl/StockControl.py
@@ -1,10 +1,14 @@
+
+
+import datetime
+
 ''' Classes'''
 
 class  StockItem(object):
     def __init__(self, uniqueID, clientName, pricePerUnit, warehouseNumber):
     
         self.uniqueID = uniqueID
-        self.clientName = clientName
+        self.__clientName = clientName
         self.pricePerUnit = pricePerUnit
         self.warehouseNumber = warehouseNumber
         
@@ -14,13 +18,87 @@ class  StockItem(object):
         return  '%d, %s, %f, %d' %(self.uniqueID, self.clientName, self.pricePerUnit, self.warehouseNumber)
 
 
-    def __add__(self, pricePerUnit):
-        ''' Perform addition with parameter 3'''
-        print 'in add' # trace print
-        sum = self.value + pricePerUnit.value
-        return StockItem(sum)
+    def __add__(self, secondItem):
+        ''' Perform addition using parameter 3'''
+        sum = self.pricePerUnit + secondItem.pricePerUnit
+        return sum
         
-        Stock1.__add__(Stock2)
+    def __mul__(self, x):
+        ''' Perform multiplication using parameter 3'''
+        sum = self.pricePerUnit * x
+        return sum
+    
+    def CalculateStorageCost(self):
+        ''' Multiplacation by .05 to get Storage Cost'''
+        sum = self.pricePerUnit * .05
+        return sum
+    
+class  CD(StockItem):
+    def __init__(self, title, dateReleased, genre, artist, uniqueID, clientName, pricePerUnit, warehouseNumber):
+        StockItem.__init__(self, uniqueID, clientName, pricePerUnit, warehouseNumber)
+    
+        self.title = title
+        self.dateReleased = dateReleased
+        self.genre = genre
+        self.artist = artist
+        
+    def __str__(self):
+        return  '%s, %d, %s, %s, %d, %f, %d' %(self.title, self.dateReleased, self.genre, self.artist, self.uniqueID, self.pricePerUnit, self.warehouseNumber)
+        
+
+    def GetDateReleased(self):
+        return '%s' %(self.dateReleased)
+
+    def IsAPreRelease(self):
+        return '%b' %(dateReleased)
+
+    def DiscountMultipleCdPurchase(self, x):
+        sum = self.pricePerUnit * x * .9
+        return sum
+
+        
+class  Book(StockItem):
+    def __init__(self, title, dateReleased, genre, author, uniqueID, clientName, pricePerUnit, warehouseNumber):
+        StockItem.__init__(self, uniqueID, clientName, pricePerUnit, warehouseNumber)
+
+        self.title = title
+        self.dateReleased = dateReleased
+        self.genre = genre
+        self.author = author
+
+    def __str__(self):
+        return  '%s, %d, %s, %s, %d, %s, %f, %d' %(self.title, self.dateReleased, self.genre, self.author, self.uniqueID, self.clientName, self.pricePerUnit, self.warehouseNumber)        
+                
+        
+        
+class  StockRepository(StockItem):
+    def __init__(self, stockCollection, uniqueID, clientName, pricePerUnit, warehouseNumber):
+        StockItem.__init__(self, uniqueID, clientName, pricePerUnit, warehouseNumber)
+
+        self.stockCollection = stockCollection
+        
+class  StockException(StockItem):
+    def __init__(self, className, methodName, dateOfOccurence, errorMessage, uniqueID, clientName, pricePerUnit, warehouseNumber):
+        StockItem.__init__(self, uniqueID, clientName, pricePerUnit, warehouseNumber)
+
+        self.className = className
+        self.methodName = methodName
+        self.dateOfOccurence = dateOfOccurence
+        self.errorMessage
+        
+        
+class  Test(object):
+    def __init__(self, createItem, calculateHoldingCost, addToRepository, positionItem, deleteItem, useCode):
+
+        self.createItem = createItem
+        self.calculateHoldingCost = calculateHoldingCost
+        self.addToRepository = addToRepository
+        self.errorMessage = errorMessage
+        self.useCode = useCode
+        
+        
+   
+    
         
 Stock1 = StockItem(702, 'Moore', 74, 879956)  
 print Stock1
